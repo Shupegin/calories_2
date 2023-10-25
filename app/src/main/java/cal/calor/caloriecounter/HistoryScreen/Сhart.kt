c75629093d9ca529bb4afbd9eedb3a765e5b14b7
dmitryshupegin <kalibri7024@gmail.com>
2023-10-25 09:08:25 +0500

    ad

--- app/src/main/java/cal/calor/caloriecounter/HistoryScreen/Сhart.kt
+++ app/src/main/java/cal/calor/caloriecounter/HistoryScreen/Сhart.kt
@@ -1,25 +1,21 @@
 package cal.calor.caloriecounter.HistoryScreen
 
-import android.util.Log
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LifecycleOwner
 import cal.calor.caloriecounter.MainViewModel
 import cal.calor.caloriecounter.ui.theme.BackgroundGray
 import co.yml.charts.axis.AxisData
 import co.yml.charts.common.model.Point
 import co.yml.charts.ui.linechart.LineChart
 
-import co.yml.charts.ui.linechart.model.GridLines
+
 import co.yml.charts.ui.linechart.model.IntersectionPoint
 import co.yml.charts.ui.linechart.model.Line
 import co.yml.charts.ui.linechart.model.LineChartData
