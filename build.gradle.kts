ff63782607118eeef2538d50d4bf3042b53c5656
dmitryshupegin <kalibri7024@gmail.com>
2024-05-13 15:22:43 +0500

    обновили гредлы

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ buildscript {
 
     dependencies {
         classpath ("com.google.gms:google-services:4.4.1")
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.4.0")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
     }
     repositories {
@@ -13,8 +13,8 @@ buildscript {
     }
 }
 plugins {
-    id ("com.android.application") version "8.3.0" apply false
-    id ("com.android.library") version "8.3.0" apply false
+    id ("com.android.application") version "8.4.0" apply false
+    id ("com.android.library") version "8.4.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id ("com.google.dagger.hilt.android") version "2.44" apply false
 }
