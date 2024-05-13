b0b3939c66f31ac348059cc8c7de0078daeadded
dmitryshupegin <kalibri7024@gmail.com>
2024-05-13 11:18:56 +0500

    Добавил отступ

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
 
     dependencies {
-        classpath ("com.google.gms:google-services:4.4.0")
+        classpath ("com.google.gms:google-services:4.4.1")
     }
     repositories {
         mavenCentral()
@@ -11,8 +11,8 @@ buildscript {
     }
 }
 plugins {
-    id ("com.android.application") version "8.1.1" apply false
-    id ("com.android.library") version "8.1.1" apply false
+    id ("com.android.application") version "8.3.0" apply false
+    id ("com.android.library") version "8.3.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id ("com.google.dagger.hilt.android") version "2.44" apply false
 }
